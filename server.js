12956386e9178604ddde1ff00a9c1c49fb5c6709
ZOD-IAC <harshitvarun89@gmail.com>
2026-05-16 14:03:08 +0530

    fix ip issue

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ import app from './src/app.js';
 
 const port = process.env.port || 5000;
 
-app.listen(port, 'localhost', () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`listening to port: ${port}`);
 });
